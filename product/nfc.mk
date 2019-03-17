3b5319a2491df8c61ea07b94d5badb846a44a99e
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-03-17 11:34:31 +0200

    include nfc permissions

--- product/nfc.mk
+++ product/nfc.mk
@@ -6,6 +6,14 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf \
 	$(DEVICE_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
 	$(DEVICE_PATH)/configs/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf
+	$(DEVICE_PATH)/configs/permissions/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml \
+        $(DEVICE_PATH)/configs/permissions/android.hardware.nfc.hcef.xml:system/vendor/etc/permissions/android.hardware.nfc.hcef.xml \
+        $(DEVICE_PATH)/configs/permissions/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
+        $(DEVICE_PATH)/configs/permissions/com.android.nfc_extras.xml:system/vendor/etc/permissions/com.android.nfc_extras.xml \
+        $(DEVICE_PATH)/configs/permissions/com.gsma.services.nfc.xml:system/vendor/etc/permissions/com.gsma.services.nfc.xml \
+        $(DEVICE_PATH)/configs/permissions/com.samsung.android.nfc.mpos.xml:$system/vendor/etc/permissions/com.samsung.android.nfc.mpos.xml \
+        $(DEVICE_PATH)/configs/permissions/com.sec.feature.cover.nfc_authentication.xml:system/vendor/etc/permissions/com.sec.feature.cover.nfc_authentication.xml \
+        $(DEVICE_PATH)/configs/permissions/com.sec.feature.nfc_authentication.xml:system/vendor/etc/permissions/com.sec.feature.nfc_authentication.xml
 
 # Packages
 PRODUCT_PACKAGES += \
